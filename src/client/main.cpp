1331e02ceaae8d077bb93708c5b9d097bb3b75a2
Konstantin Vasilev-Bagler <konstantin.bagler@rimac-technology.com>
2024-05-18 11:07:13 +0200

    create separate source dirs for client and server

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world" << std::endl;
+	std::cout << "Hello from client" << std::endl;
 
 	return 0;
 }
---
+++ src/server/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello from server" << std::endl;
+
+	return 0;
+}
+
